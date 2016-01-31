263a4cc39814dfa6eaa3a3d727ed00600e59fc6e
Katie Ning <katiening@dhcp-gs-2440.redrover.cornell.edu>
2016-01-31 14:55:01 -0500

    delete the incorrect views and working on the launch view

--- CloudyBird/CloudyBird/ViewController.swift
+++ CloudyBird/CloudyBird/ViewController.swift
@@ -14,7 +14,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //this is the title of the game
+        let NameImage = UIImage(named: "GameName")
+        let NameImageView = UIImageView(image: NameImage!)
+        NameImageView.frame = CGRect(x: 0, y: 50, width: 420, height: 45)
+        view.addSubview(NameImageView)
+        
+        //this is the start button of the game
+        let startImage = UIImage(named: "StartGame")
+        let startImageView = UIImageView(image: startImage!)
+        startImageView.frame = CGRect(x: 0, y: 300, width: 150, height: 50)
+        startImageView.center = CGPoint(x: view.frame.midX, y: 300)
+        
+        //create gesture actions for the image
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("startTapped:"))
+        startImageView.userInteractionEnabled = true
+        startImageView.addGestureRecognizer(tapGestureRecognizer)
+        view.addSubview(startImageView)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func startTapped(img: AnyObject){
+        print("image tapped")
         
     }
 
