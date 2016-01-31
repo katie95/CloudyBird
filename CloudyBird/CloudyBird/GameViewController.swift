869ee9c93b7f251a09fd241a0801b1a2b180085d
Katie Ning <katiening@dhcp-gs-2440.redrover.cornell.edu>
2016-01-31 15:21:07 -0500

    transition between view controllers

---
+++ CloudyBird/CloudyBird/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  CloudyBird
+//
+//  Created by Katie Ning on 1/31/16.
+//  Copyright © 2016 Katie Ning. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+        view.backgroundColor = UIColor.blueColor()
+        //this is the back button
+        let backImage = UIImage(named: "Back")
+        let backImageView = UIImageView(image: backImage!)
+        backImageView.frame = CGRect(x: view.frame.maxX-50, y: 0, width: 50, height: 50)
+        
+        //create gesture actions for the image
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("backTapped:"))
+        backImageView.userInteractionEnabled = true
+        backImageView.addGestureRecognizer(tapGestureRecognizer)
+        view.addSubview(backImageView)
+        // Do any additional setup after loading the view.
+    }
+    
+    func backTapped(img: AnyObject){
+        //print("back tapped")
+        let launchVC = ViewController()
+        
+        presentViewController(launchVC, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CloudyBird/CloudyBird/ViewController.swift
+++ CloudyBird/CloudyBird/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+        view.backgroundColor = UIColor.whiteColor()
         //this is the title of the game
         let NameImage = UIImage(named: "GameName")
         let NameImageView = UIImageView(image: NameImage!)
@@ -33,14 +34,30 @@ class ViewController: UIViewController {
         startImageView.addGestureRecognizer(tapGestureRecognizer)
         view.addSubview(startImageView)
         
+        //this is the button for the rule
+        let ruleImage = UIImage(named: "Rule")
+        let ruleImageView = UIImageView(image: ruleImage!)
+        ruleImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        ruleImageView.center = CGPoint(x: view.frame.midX, y: 400)
+        let ruleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("ruleTapped:"))
+        ruleImageView.userInteractionEnabled = true
+        ruleImageView.addGestureRecognizer(ruleTapGestureRecognizer)
+        view.addSubview(ruleImageView)
         // Do any additional setup after loading the view, typically from a nib.
         
     }
     
     func startTapped(img: AnyObject){
-        print("image tapped")
+        let gameVC = GameViewController()
+        presentViewController(gameVC, animated: true, completion: nil)
         
     }
+    
+    func ruleTapped(img: AnyObject){
+        print("rule tapped")
+        //plan to add a subview, with animation
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
