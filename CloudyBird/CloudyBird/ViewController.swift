c19b3553059e3c5f87c45ca1acceea4ae7bd22bc
Katie Ning <katiening@dhcp-mcfad-468.redrover.cornell.edu>
2016-02-01 22:20:54 -0500

    make the launch screen looks better

--- CloudyBird/CloudyBird/ViewController.swift
+++ CloudyBird/CloudyBird/ViewController.swift
@@ -16,16 +16,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
         view.backgroundColor = UIColor.whiteColor()
+        
+        //this is the background picture
+        let bgImage = UIImage(named: "background1")
+        let bgImageView = UIImageView(image: bgImage!)
+        bgImageView.frame = view.frame
+        view.addSubview(bgImageView)
+        
         //this is the title of the game
         let NameImage = UIImage(named: "GameName")
         let NameImageView = UIImageView(image: NameImage!)
         NameImageView.frame = CGRect(x: 20, y: 150, width: 365, height: 190)
         view.addSubview(NameImageView)
         
+        let rainbowImage = UIImage(named: "Rainbow")
+        let rainbowImageView = UIImageView(image: rainbowImage!)
+        rainbowImageView.frame = CGRect(x: view.frame.midX, y: 450, width: 300, height: 300)
+        rainbowImageView.center = CGPoint(x: view.frame.midX, y: 450)
+        //view.addSubview(rainbowImageView)
+        
         //this is the button for the rule
         let ruleImage = UIImage(named: "Rule")
         let ruleImageView = UIImageView(image: ruleImage!)
-        ruleImageView.frame = CGRect(x: view.frame.midX-50, y: 450, width: 300, height: 300)
+        ruleImageView.frame = CGRect(x: view.frame.midX - 20, y: 450, width: 225, height: 150)
         let ruleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("ruleTapped:"))
         ruleImageView.userInteractionEnabled = true
         ruleImageView.addGestureRecognizer(ruleTapGestureRecognizer)
@@ -35,8 +48,7 @@ class ViewController: UIViewController {
         //this is the start button of the game
         let startImage = UIImage(named: "StartGame")
         let startImageView = UIImageView(image: startImage!)
-        startImageView.frame = CGRect(x: -40, y: 350, width: 300, height: 300)
-        
+        startImageView.frame = CGRect(x: view.frame.midX - 200, y: 350, width: 225, height: 150)
         
         //create gesture actions for the image
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("startTapped:"))
